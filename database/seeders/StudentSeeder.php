2face269df1a2bc37b0fd3932152fdb54adef2e6
Roberth Rodriguez <rrodriguezj@uni.pe>
2022-05-11 00:42:31 -0500

    New students seeder

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Grade;
 use App\Models\Student;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -32,12 +33,49 @@ class StudentSeeder extends Seeder
         $users = User::factory()
             ->count(1)
             ->create([
-                'name' => 'Student',
-                'email' => 'student@mail.com',
+                'name' => 'Student 1',
+                'email' => 'student1@mail.com',
                 'password' => '$2y$10$nK4EQKL2hz1e6lMNsGUw5O/8lRjCdPRSo6e/te3GgrqUz9sTsl/qG' //secret
             ])->each(function (\App\Models\User $u) {
                 Student::factory([
-                    'user_id' => $u->id
+                    'user_id' => $u->id,
+                    'grade_id' => Grade::FIRST_HIGH_SCHOOL
+                ])
+                    ->count(1)
+                    ->create();
+            });
+        foreach ($users as $user){
+            $user->assignRole('student');
+        }
+
+        $users = User::factory()
+            ->count(1)
+            ->create([
+                'name' => 'Student 2',
+                'email' => 'student2@mail.com',
+                'password' => '$2y$10$nK4EQKL2hz1e6lMNsGUw5O/8lRjCdPRSo6e/te3GgrqUz9sTsl/qG' //secret
+            ])->each(function (\App\Models\User $u) {
+                Student::factory([
+                    'user_id' => $u->id,
+                    'grade_id' => Grade::FOURTH_MIDDLE_SCHOOL
+                ])
+                    ->count(1)
+                    ->create();
+            });
+        foreach ($users as $user){
+            $user->assignRole('student');
+        }
+
+        $users = User::factory()
+            ->count(1)
+            ->create([
+                'name' => 'Student 3',
+                'email' => 'student3@mail.com',
+                'password' => '$2y$10$nK4EQKL2hz1e6lMNsGUw5O/8lRjCdPRSo6e/te3GgrqUz9sTsl/qG' //secret
+            ])->each(function (\App\Models\User $u) {
+                Student::factory([
+                    'user_id' => $u->id,
+                    'grade_id' => Grade::FIRST_HIGH_SCHOOL
                 ])
                     ->count(1)
                     ->create();
