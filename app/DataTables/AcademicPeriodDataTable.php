3554acb3d0fa56cce82a6269e5d2e5466ec53a06
Roberth Rodriguez <rrodriguezj@uni.pe>
2022-06-11 09:31:42 -0500

    Laravel PDF generator

--- app/DataTables/AcademicPeriodDataTable.php
+++ app/DataTables/AcademicPeriodDataTable.php
@@ -77,7 +77,7 @@ class AcademicPeriodDataTable extends DataTable
                                             </svg> '.__('Excel')
                                 )
                                 ->titleAttr('Excel'),
-                            Button::make('print')
+                            Button::make('pdf')
                                 ->className('dropdown-item')
                                 ->text('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-printer">
                                             <polyline points="6 9 6 2 18 2 18 9"></polyline>
--- app/DataTables/EnrollmentDataTable.php
+++ app/DataTables/EnrollmentDataTable.php
@@ -50,18 +50,62 @@ class EnrollmentDataTable extends DataTable
     public function html()
     {
         return $this->builder()
-                    ->setTableId('enrollment-table')
-                    ->columns($this->getColumns())
-                    ->minifiedAjax()
-                    ->dom('Bfrtip')
-                    ->orderBy(1)
-                    ->buttons(
-                        Button::make('create'),
-                        Button::make('export'),
-                        Button::make('print'),
-                        Button::make('reset'),
-                        Button::make('reload')
-                    );
+            ->parameters([
+                'paging' => true,
+                'lengthMenu'  => [[10, 20, 50, 100], [10, 20, 50, 100]],
+                'searchDelay' => 1000,
+            ])
+            ->language([
+                'url' => '//cdn.datatables.net/plug-ins/1.10.16/i18n/'.config('languages')[session('applocale')][0].'.json',
+            ])
+            ->setTableId('course-table')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            ->dom('Blfrtip')
+            ->orderBy(0)
+            ->buttons(
+                Button::make([])
+                    ->extend('collection')
+                    ->className('btn btn-outline-secondary dropdown-toggle me-2')
+                    ->text('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-external-link">
+                                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                                        <polyline points="15 3 21 3 21 9"></polyline>
+                                        <line x1="10" y1="14" x2="21" y2="3"></line>
+                                    </svg> '.__('Export'))
+                    ->buttons([
+                            Button::make('excel')
+                                ->className('dropdown-item')
+                                ->text('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text">
+                                                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                                <polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line>
+                                                <line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline>
+                                            </svg> '.__('Excel')
+                                )
+                                ->titleAttr('Excel'),
+                            Button::make('pdf')
+                                ->className('dropdown-item')
+                                ->text('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-printer">
+                                            <polyline points="6 9 6 2 18 2 18 9"></polyline>
+                                            <path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"></path>
+                                            <rect x="6" y="14" width="12" height="8"></rect>
+                                        </svg> '.__('Print')
+                                ),
+                            Button::make('reload')
+                                ->text('<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-loader">
+                                            <line x1="12" y1="2" x2="12" y2="6"></line>
+                                            <line x1="12" y1="18" x2="12" y2="22"></line>
+                                            <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
+                                            <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line>
+                                            <line x1="2" y1="12" x2="6" y2="12"></line>
+                                            <line x1="18" y1="12" x2="22" y2="12"></line>
+                                            <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line>
+                                            <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
+                                        </svg> '.__('Reload')
+                                )
+                                ->className('dropdown-item'),
+                        ]
+                    )
+            );
     }
 
     /**
