a4f050fee2f2573072761c1f944b045f48ef178c
Roberth Rodriguez <rrodriguezj@uni.pe>
2022-06-30 16:32:21 -0500

    Fix grade

--- resources/views/student/show.blade.php
+++ resources/views/student/show.blade.php
@@ -25,8 +25,8 @@
                                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check font-medium-2"><polyline points="20 6 9 17 4 12"></polyline></svg>
                                 </span>
                                 <div class="ms-75">
-                                    <h4 class="mb-0">{{ __($student->grade->name) }}</h4>
-                                    <small>{{ $student->grade->level->custom_name }}</small>
+                                    <h4 class="mb-0">{{ $student->grade ? __($student->grade->name) : null }}</h4>
+                                    <small>{{ $student->grade ? $student->grade->level->custom_name : null }}</small>
                                 </div>
                             </div>
                         </div>
