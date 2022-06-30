4c9c097f22387111600df618e4724cb1267e04dc
Roberth Rodriguez <rrodriguezj@uni.pe>
2022-06-30 17:19:50 -0500

    Test 01

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,64 @@
                 max-height: 45px;
             }
         </style>
+
+
+        <link href="{{ asset('drag-and-drop/demo.css')}}" rel='stylesheet' type='text/css'>
+        <link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+        <link href="{{ asset('drag-and-drop/draganddrop.css') }}" rel='stylesheet' type='text/css'>
+        <link href="https://bootswatch.com/flatly/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+        <script src='{{ asset('drag-and-drop/draganddrop.js') }}' type='text/javascript'></script>
+        <style>
+            body { background-color:#fafafa;}
+            .container { margin:150px auto;}
+        </style>
+        <script type='text/javascript'>
+            $(function() {
+                //grouped lists
+                $('ul.grouped').sortable({
+                    group: true
+                });
+
+                //normal list
+                $('ul.normal').sortable({
+                    autocreate: true,
+                    update: function(evt) {
+                        console.log(JSON.stringify($(this).sortable('serialize')));
+                    }
+                });
+
+                //remaining lists
+                $('ul.float, ul.inline').sortable({
+                    update: function(evt) {
+                        console.log(JSON.stringify($(this).sortable('serialize')));
+                    }
+                });
+
+                //div list
+                $('.list.parent').sortable({container: '.list', nodes: ':not(.list)'});
+
+                //draggable
+                $('.drag').draggable();
+                $('.draggables').draggable({delegate: 'button', placeholder: true});
+                $('.draghandle').draggable({handle: '.handle', placeholder: true});
+                $('.dragdrop').draggable({
+                    revert: true,
+                    placeholder: true,
+                    droptarget: '.drop',
+                    drop: function(evt, droptarget) {
+                        $(this).appendTo(droptarget).draggable('destroy');
+                    }
+                });
+
+                //off switch
+                $('.off').on('click', function() {
+                    $('.sortable').each(function() { $(this).sortable('destroy'); });
+                    $('.draggable').each(function() { $(this).draggable('destroy'); });
+                });
+            });
+        </script>
+
     </head>
     <!-- END: Head-->
 
@@ -69,27 +127,21 @@
         <div class="sidenav-overlay"></div>
         <div class="drag-target"></div>
 
-        {{-- include footer --}}
-        @include('panels.footer')
-        <script src="{{ asset('vendors/js/vendors.min.js') }}"></script>
-        <script src="{{ asset('vendors/js/ui/jquery.sticky.js') }}"></script>
-        @stack('vendor-scripts')
-        <script src="{{ asset('js/core/app-menu.js') }}"></script>
-        <script src="{{ asset('js/core/app.js') }}"></script>
-        <script src="{{ asset('js/core/scripts.js') }}"></script>
-        <script src="{{ asset('js/scripts/customizer.js') }}"></script>
-        <script src="{{ asset('js/scripts/utils.js') }}"></script>
+        
 
         <script type="text/javascript">
-            $(window).on('load', function() {
 
-                if (feather) {
-                    feather.replace({
-                        width: 14, height: 14
-                    });
-                }
-            })
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-36251023-1']);
+            _gaq.push(['_setDomainName', 'jqueryscript.net']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+
         </script>
-        @stack('scripts')
     </body>
 </html>
