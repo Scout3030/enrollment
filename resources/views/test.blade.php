1bb69c79bd7e1a56ce64cd6d134ccb13c9b8ff97
Roberth Rodriguez <rrodriguezj@uni.pe>
2022-06-30 20:47:54 -0500

    Test 05

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,7 +9,120 @@
     <h3>Draggables</h3>
 
 
-    <h3>Off Switch</h3>
+    <section id="card-content-types">
+        <h5 class="mt-3">Content Types</h5>
+
+        <div class="row">
+            <div class="col-md-6 col-lg-4">
+                <h6 class="my-2 text-muted">Body</h6>
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <p class="card-text">
+                            This is some text within a card body. Jelly lemon drops tiramisu chocolate cake cotton candy soufflé oat
+                            cake sweet roll. Sugar plum marzipan dragée topping cheesecake chocolate bar. Danish muffin icing donut.
+                        </p>
+                    </div>
+                </div>
+                <h6 class="my-2 text-muted">Titles, Text, and Links</h6>
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <div class="card-subtitle text-muted mb-1">Card subtitle</div>
+                        <p class="card-text">
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+                <h6 class="my-2 text-muted">List Groups</h6>
+                <div class="card mb-4">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <h6 class="my-2 text-muted">Images</h6>
+                <div class="card mb-4">
+                    <img class="card-img-top" src="../../../app-assets/images/slider/01.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                        <p class="card-text">
+                            Cookie topping caramels jujubes gingerbread. Lollipop apple pie cupcake candy canes cookie ice cream. Wafer
+                            chocolate bar carrot cake jelly-o.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <h6 class="my-2 text-muted">Kitchen Sink</h6>
+                <div class="card">
+                    <img class="card-img-top" src="../../../app-assets/images/slider/02.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <h6 class="my-2 text-muted">Header and Footer</h6>
+        <div class="row">
+            <div class="col-md-6 col-lg-4">
+                <div class="card">
+                    <div class="card-header">Featured</div>
+                    <div class="card-body">
+                        <h4 class="card-title">Special title treatment</h4>
+                        <p class="card-text">
+                            With supporting text below as a natural lead-in to additional content natural lead-in to additional content.
+                        </p>
+                        <a href="#" class="btn btn-outline-primary waves-effect">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="card">
+                    <h6 class="card-header">Quote</h6>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit
+                                amet, Integer posuere erat a ante Integer posuere erat a anteconsectetur.
+                            </p>
+                            <footer class="blockquote-footer">
+                                Someone famous in
+                                <cite title="Source Title">Source Title</cite>
+                            </footer>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="card text-center">
+                    <div class="card-header">Featured</div>
+                    <div class="card-body">
+                        <h4 class="card-title">Special title treatment</h4>
+                        <p class="card-text">With supporting text below as a natural.</p>
+                        <a href="#" class="btn btn-outline-primary waves-effect">Go somewhere</a>
+                    </div>
+                    <div class="card-footer text-muted">2 days ago</div>
+                </div>
+            </div>
+        </div>
+    </section>
     <button class="btn btn-primary off">All off</button>
 
     <div class="row">
@@ -51,6 +164,120 @@
             </div>
         </div>
     </div>
+    <section id="card-content-types">
+        <h5 class="mt-3">Content Types</h5>
+
+        <div class="row">
+            <div class="col-md-6 col-lg-4">
+                <h6 class="my-2 text-muted">Body</h6>
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <p class="card-text">
+                            This is some text within a card body. Jelly lemon drops tiramisu chocolate cake cotton candy soufflé oat
+                            cake sweet roll. Sugar plum marzipan dragée topping cheesecake chocolate bar. Danish muffin icing donut.
+                        </p>
+                    </div>
+                </div>
+                <h6 class="my-2 text-muted">Titles, Text, and Links</h6>
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <div class="card-subtitle text-muted mb-1">Card subtitle</div>
+                        <p class="card-text">
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+                <h6 class="my-2 text-muted">List Groups</h6>
+                <div class="card mb-4">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Dapibus ac facilisis in</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <h6 class="my-2 text-muted">Images</h6>
+                <div class="card mb-4">
+                    <img class="card-img-top" src="../../../app-assets/images/slider/01.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <p class="card-text">
+                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                        </p>
+                        <p class="card-text">
+                            Cookie topping caramels jujubes gingerbread. Lollipop apple pie cupcake candy canes cookie ice cream. Wafer
+                            chocolate bar carrot cake jelly-o.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <h6 class="my-2 text-muted">Kitchen Sink</h6>
+                <div class="card">
+                    <img class="card-img-top" src="../../../app-assets/images/slider/02.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Cras justo odio</li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <h6 class="my-2 text-muted">Header and Footer</h6>
+        <div class="row">
+            <div class="col-md-6 col-lg-4">
+                <div class="card">
+                    <div class="card-header">Featured</div>
+                    <div class="card-body">
+                        <h4 class="card-title">Special title treatment</h4>
+                        <p class="card-text">
+                            With supporting text below as a natural lead-in to additional content natural lead-in to additional content.
+                        </p>
+                        <a href="#" class="btn btn-outline-primary waves-effect">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="card">
+                    <h6 class="card-header">Quote</h6>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.Lorem ipsum dolor sit
+                                amet, Integer posuere erat a ante Integer posuere erat a anteconsectetur.
+                            </p>
+                            <footer class="blockquote-footer">
+                                Someone famous in
+                                <cite title="Source Title">Source Title</cite>
+                            </footer>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <div class="card text-center">
+                    <div class="card-header">Featured</div>
+                    <div class="card-body">
+                        <h4 class="card-title">Special title treatment</h4>
+                        <p class="card-text">With supporting text below as a natural.</p>
+                        <a href="#" class="btn btn-outline-primary waves-effect">Go somewhere</a>
+                    </div>
+                    <div class="card-footer text-muted">2 days ago</div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
 
 @push('scripts')
