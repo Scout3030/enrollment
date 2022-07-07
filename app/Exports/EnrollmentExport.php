f3aaf9bc778e630ea61a290b75382f30b69ee0aa
tony figueras <tonfy01@gmail.com>
2022-07-07 16:03:40 -0400

    fix excel enrollment

--- app/Exports/EnrollmentExport.php
+++ app/Exports/EnrollmentExport.php
@@ -3,6 +3,8 @@
 namespace App\Exports;
 
 use App\Models\Enrollment;
+use App\Models\Grade;
+use App\Models\CourseType;
 use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
@@ -71,7 +73,28 @@ class EnrollmentExport implements FromCollection, WithMapping, WithHeadings
             __('DNI'),
             __('Level'),
             __('Grade'),
-            __('Courses'),
+          //   THIRD_HIGH_SCHOOL
+            __('Ámbitos comunes'),
+          //  FIRST_MIDDLE_SCHOOL - THIRD_MIDDLE_SCHOOL
+            __('Materias comunes'),
+            __('Materias optativas (Númeradas por orden de preferencia)'),
+            __('Materias optativas (Seleccionada)'),
+          //   SECOND_MIDDLE_SCHOOL - SECOND_HIGH_SCHOOL
+            __('Troncales'),
+            __('Específicas (Obligatorias)'),
+            __('Específica (Seleccionada)'),
+            __('Libre configuración (Númeradas por orden de preferencia)'),
+         //   FOURTH_MIDDLE_SCHOOL
+            __('Cursos itinerarios'),
+            __('Cursos específicos itineraros'),
+            __('Cursos libre configuración itineraros'),
+        // FIRST_EDUCATIONAL_CYCLE_BASIC - SECOND_EDUCATIONAL_CYCLE_BASIC
+            __('MODULES ASSOCIATED WITH UNITS OF COMPETENCE'),
+            __('MODULES ASSOCIATED WITH COMMON BLOCKS'),
+            __('TRAINING MODULES IN WORK CENTERS'),
+        // FIRST_EDUCATIONAL_CYCLE_MEDIUM - SECOND_EDUCATIONAL_CYCLE_MEDIUM
+           __('courses required'),
+
             __('Registered at'),
         ];
     }
@@ -79,13 +102,94 @@ class EnrollmentExport implements FromCollection, WithMapping, WithHeadings
     public function map($row): array
     {
         $student = $row['student']['user']['name']." ".$row['student']['middle_name']." ".$row['student']['paternal_surname']." ".$row['student']['maternal_surname'];
-        $list = '';
+        $list1 = '';$list2 = '';$list3 = '';$list4 = '';$list5 = '';$list6 = '';$list7 = '';$list8 = '';$list8 = '';$list9 = '';
+        $list10 = '';$list11 = '';$list12 = '';$list13 = '';$list14 = '';$list15 = '';
+       
         foreach ($row['courses'] as $key => $course){
-            if($key+1 == count($row['courses'])){
-                $list .= $course['name'];
-            } else {
-                $list .= $course['name']."\r\n";
+           // dd($row['courses'],$course['name'],$course['grade_id'],$course['course_type_id'],$course['pivot']['order']);
+           
+            if ($course['grade_id'] == Grade::FIRST_MIDDLE_SCHOOL || $course['grade_id'] == Grade::THIRD_MIDDLE_SCHOOL
+                || $course['grade_id'] == Grade::THIRD_HIGH_SCHOOL){
+                if(CourseType::COMMON == $course['course_type_id']){
+                    $list1 .= $course['name']."\r\n";
+                }
+                if(CourseType::COMMON_OPTIONAL_ONE == $course['course_type_id']){
+                    $list2 .= $course['pivot']['order'].". ".$course['name']."\r\n";
+                }
+                if(CourseType::COMMON_OPTIONAL_TWO == $course['course_type_id']){
+                    $list3 .= $course['name']."\r\n";
+                }
+                if(CourseType::COMMON_AREAS == $course['course_type_id']){
+                    $list8 .= $course['name']."\r\n";
+                }
+            }else{
+                $list1 .= '-';
+                $list2 .= '-';
+                $list3 .= '-';
+                $list8 .= '-';
             }
+
+            if ($course['grade_id'] == Grade::SECOND_MIDDLE_SCHOOL || $course['grade_id'] == Grade::SECOND_HIGH_SCHOOL ||
+            $course['grade_id'] == Grade::FOURTH_MIDDLE_SCHOOL){
+                if(CourseType::CORE == $course['course_type_id']){
+                    $list4 .= $course['name']."\r\n";
+                }
+                if(CourseType::SPECIFIC == $course['course_type_id']){
+                    $list5 .= $course['name']."\r\n";
+                }
+                if(CourseType::SPECIFIC_FREE_CONFIGURATION == $course['course_type_id']){
+                    $list6 .= $course['name']."\r\n";
+                }
+                if(CourseType::FREE_CONFIGURATION == $course['course_type_id']){
+                    $list7 .= $course['pivot']['order'].". ".$course['name']."\r\n";
+                }
+                if(CourseType::ITINERARY == $course['course_type_id']){
+                    $list9 .= $course['name']."\r\n";
+                }
+                if(CourseType::SPECIFIC_ITINERARY == $course['course_type_id']){
+                    $list10 .= $course['pivot']['order'].". ".$course['name']."\r\n";
+                }
+                if(CourseType::FREE_CONFIGURATION_ITINERARY == $course['course_type_id']){
+                    $list11 .= $course['pivot']['order'].". ".$course['name']."\r\n";
+                }
+            }else{
+                $list4 .= '-';
+                $list5 .= '-';
+                $list6 .= '-';
+                $list7 .= '-';
+                $list9 .= '-';
+                $list10 .= '-';
+                $list11 .= '-';
+            }
+
+            if ($course['grade_id'] == Grade::FIRST_EDUCATIONAL_CYCLE_BASIC || $course['grade_id'] == Grade::SECOND_EDUCATIONAL_CYCLE_BASIC){
+                if(CourseType::ASSOCIATED_UNITS_OF_COMPETENCES == $course['course_type_id']){
+                    $list12 .= $course['name']."\r\n";
+                }
+                if(CourseType::ASSOCIATED_COMMON_BLOCKS == $course['course_type_id']){
+                    $list13 .= $course['name']."\r\n";
+                }
+                if(CourseType::FORMATION_WORKSPACE == $course['course_type_id']){
+                    $list14 .= $course['pivot']['order'].". ".$course['name']."\r\n";
+                }
+                
+            }else{
+                $list12 .= '-';
+                $list13 .= '-';
+                $list14 .= '-';
+             
+            }
+            
+            if ($course['grade_id'] == Grade::FIRST_EDUCATIONAL_CYCLE_MEDIUM || $course['grade_id'] == Grade::SECOND_EDUCATIONAL_CYCLE_MEDIUM){
+                if(CourseType::CF_COMMON == $course['course_type_id']){
+                    $list15 .= $course['name']."\r\n";
+                }    
+                
+            }else{
+                $list15 .= '-';              
+             
+            }
+         
         }
 
         return [
@@ -95,7 +199,22 @@ class EnrollmentExport implements FromCollection, WithMapping, WithHeadings
             $row['student']['dni'],
             $row['grade']['level']['custom_name'],
             $row['grade']['name'],
-            $list,
+            $list8,
+            $list1,
+            $list2,
+            $list3,
+            $list4,
+            $list5,
+            $list6,
+            $list7,
+            $list9,
+            $list10,
+            $list11,
+            $list12,
+            $list13,
+            $list14,
+            $list15,      
+
             $row['created_at']->format('Y-m-d H:i'),
         ];
     }
